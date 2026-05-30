6c71f0e68c58ff40b64f4b91ccf9c9047de714a0
Ahmed <ahmedmm72005@gmail.com>
2026-05-31 02:22:55 +0300

    app-release 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.axoncodelabs.fundflow"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
