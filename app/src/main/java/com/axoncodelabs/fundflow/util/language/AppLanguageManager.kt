205e57aaf4bd8bf75a3781a62e37dfd50e933e00
Ahmed <ahmedmm72005@gmail.com>
2026-08-31 00:43:04 +0300

    Feat: Add animated cover for language switching

--- app/src/main/java/com/axoncodelabs/fundflow/util/language/AppLanguageManager.kt
+++ app/src/main/java/com/axoncodelabs/fundflow/util/language/AppLanguageManager.kt
@@ -1,5 +1,6 @@
 package com.axoncodelabs.fundflow.util.language
 
+import androidx.compose.runtime.mutableStateOf
 import java.util.Locale
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -11,15 +12,21 @@ class AppLanguageManager @Inject constructor() {
     var currentLanguage: Language = Language.English
         private set
 
+    var isLanguageSwitching = mutableStateOf(false)
+        private set
+
     fun initialize(language: Language) {
         currentLanguage = language
-
         appLocale = Locale.forLanguageTag(language.languageTag)
     }
 
     fun setLanguage(language: Language) {
+        isLanguageSwitching.value = true
         currentLanguage = language
-
         appLocale = Locale.forLanguageTag(language.languageTag)
+    }
+
+    fun finishLanguageSwitch() {
+        isLanguageSwitching.value = false
     }
 }
